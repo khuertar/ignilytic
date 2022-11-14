d50326a684288fbf7b5051897027b712d505b509
VMRR <m.ramirez@ignilytic.com>
2022-11-14 00:59:54 -0600

    Se crea pagina servicios

---
+++ view/pages/servicios.php
@@ -0,0 +1,46 @@
+<section class="servicios">
+
+  <div class="general__titulo"><h1>Nuestros servicios</h1></div>
+
+  <div class="servicios_in">
+
+    <div class="general__card servicio__Web servicio__item">
+      <h3>Páginas Web</h3>
+      <div class="servicio_img01"></div>
+      <p>El tener una página web te ayudara a cruzar las fronteras y dar a conocer tu servicios y productos.</p>
+      <a href="web" class="btn__ver">Ver</a>
+    </div>
+
+    <div class="general__card servicio__ti servicio__item">
+      <h3>Reparación Equipos</h3>
+      <div class="servicio_img02"></div>
+      <p> Manten tu equipo de cómputo siempre en buenas condiciones.</p>
+      <a href="ti" class="btn__ver">Ver</a>
+    </div>
+
+    <div class="general__card servicio__diseno servicio__item">
+      <h3>Diseño Gráfico</h3>
+      <div class="servicio_img03"></div>
+      <p> Crea una marca personal y posicionala entre las mejores.</p>
+      <a href="diseno" class="btn__ver">Ver</a>
+    </div>
+
+    <div class="general__card servicio__software servicio__item">
+      <h3>Software Personalizado</h3>
+      <div class="servicio_img04"></div>
+      <p> Software a la medida para tu mejorar la productividad, calidad y servicio.</p>
+      <a href="aplicaciones" class="btn__ver">Ver</a>
+    </div>
+
+  </div>
+
+  <div class="general__titulo"><h2>Equipo de trabajo</h2></div>
+
+  <div class="servicios__info">
+    <p>Contamos con especialistas en cada una de las categorías que ofrecemos, por lo que ponemos al alcance de tus manos servicios de alta tecnología.</p>
+    <p>Nuestro compromiso es ayudar desde un pequeño proyecto hasta la infraestructura de una gran empresa, con la experiencia obtenida a lo largo de nuestro servicio podemos identificar las necesidades que requiere cada uno de nuestros clientes.</p>
+    <p>Ayudándolos a minimizar gastos y aprovechar los servicios con los que ya cuentan, integrando todo en su proyecto.</p>
+  </div>
+
+</section>
+
--- view/template.php
+++ view/template.php
@@ -47,7 +47,8 @@
       //* ===============================================
       if(isset($_GET["url"])){
 
-        if($_GET["url"] == "inicio" 
+        if($_GET["url"] == "inicio" ||
+           $_GET["url"] == "servicios"
          
         ){
 
