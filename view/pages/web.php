176ee34e5df7d2ee9384b4b7bd1c42f534a397d1
VMRR <m.ramirez@ignilytic.com>
2022-11-15 00:13:03 -0600

    Se actualiza footer

---
+++ view/pages/web.php
@@ -0,0 +1,107 @@
+<section class="web">
+
+  <div class="general__titulo"><h1>Página Web</h1></div>
+
+  <div class="web__paquetes">
+    <div class="web_card general__card web__economico">
+      <h3>Economico</h3>
+      <h3>$4,000</h3>
+      <div class="web__text">
+        <span>Tipo landing page</span>
+        <span>1 sección</span>
+        <span>Hasta 2 imágenes por parte el cliente</span>
+        <span>2 imágenes de stock</span>
+        <span>1 Propuesta de diseño</span>
+        <span>2 Correos empresariales de 500 Mb</span>
+        <span>Tiempo de entrega aproximado 5 - 7 días habiles</span>
+        <span>Espacio en disco duro 5,000 Mb</span>
+      </div>
+      <a href="contacto" class="btn__ver">Contratar</a>
+    </div>
+
+    <div class="web_card general__card web__emprendedor">
+      <h3>Emprendedor</h3>
+      <h3>$7,000</h3>
+      <div class="web__text">
+        <span>Página web informativa</span>
+        <span>3 - 6 seccione</span>
+        <span>Hasta 10 imágenes por parte el cliente</span>
+        <span>5 imágenes de stock</span>
+        <span>1 Propuesta de diseño</span>
+        <span>5 Correos empresariales de 500 Mb</span>
+        <span>TTiempo de entrega aproximado 7-10 días habiles</span>
+        <span>Espacio en disco duro 10,000 Mb</span>
+      </div>
+      <a href="contacto" class="btn__ver">Contratar</a>
+    </div>
+
+    <div class="web_card general__card web__micro">
+      <h3>Microempresa</h3>
+      <h3>$13,000</h3>
+      <div class="web__text">
+        <span>Página web informativa / servicios</span>
+        <span>7 - 12 secciónes</span>
+        <span>Hasta 25 imágenes por parte el cliente</span>
+        <span>8 imágenes de stock</span>
+        <span>1 Propuesta de diseño</span>
+        <span>10 Correos empresariales de 500 Mb</span>
+        <span>Tiempo de entrega aproximado10 - 15 días habiles</span>
+        <span>Espacio en disco duro 15,000 Mb</span>
+      </div>
+      <a href="contacto" class="btn__ver">Contratar</a>
+    </div>
+
+    <div class="web_card general__card web__auto">
+      <h3>Sitio web auto administrable</h3>
+      <h3>$10,000</h3>
+      <div class="web__text">
+        <span>No dependas de alguien</span>
+        <span>para actualizar tu propio sitio web</span>
+        <span>contrata este servicio</span>
+        <span>agrega el costo a cualquier</span>
+        <span>servicio ya contratado.</span>
+        <span><b>Se independiente</b></span>
+      </div>
+      <a href="contacto" class="btn__ver">Contratar</a>
+    </div>
+  </div>
+
+  <div class="general__titulo">
+    <h2>
+      Nuestros proyectos cuentan con las siguientes
+      características
+    </h2>
+  </div>
+
+  <div class="web__info">
+    <div class="web__imgInfo"></div>
+    <!-- <a href="#" class="btn__ver web__btnInfo" data-bs-toggle="modal" data-bs-target="#modalInfo">Información</a> -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalInfo">
+      Launch demo modal
+    </button>
+  </div>
+
+  <!--===============================================
+  Modal
+  =================================================-->
+  <!-- Modal -->
+<div class="modal fade" id="modalInfo" tabindex="-1" role="dialog" aria-labelledby="modalInfo" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
--- view/template.php
+++ view/template.php
@@ -10,17 +10,15 @@
   =================================================-->
   <link rel="stylesheet" href="view/src/css/main.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
-  <!-- Boostrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>  
   <!-- Carrusel -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css"/>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"/>
+  <!-- Normalize -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"/>
   <!--===============================================
   lib js
-  =================================================-->
-  <!-- JavaScript Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+  =================================================-->  
   <!-- Jquery -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <!-- Carousel -->
@@ -48,7 +46,8 @@
       if(isset($_GET["url"])){
 
         if($_GET["url"] == "inicio" ||
-           $_GET["url"] == "servicios"
+           $_GET["url"] == "servicios" ||
+           $_GET["url"] == "web"
          
         ){
 
